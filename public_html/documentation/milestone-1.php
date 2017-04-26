fe72dac59dc523b02f3867778dc95dee881bf258
Marcus Lester <mlester3@cnm.edu>
2017-04-26 17:06:35 -0600

    Created html for Milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,43 @@
+<!Doctype>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Marcus Lester's PWP</title>
+	</head>
+		<body>
+
+			<main>
+<h1>Bad Etsy PWP Milestone 1</h1>
+				<h3>Purpose:</h3>
+				<p>This project will be a website for my business A.N.C. Stuidios The main focus is to use front-end web development in creating the website.</p>
+				<h3>Audience:</h3>
+				<p>This site is aimed towards professionals in the event planning/entertainment industry, looking for Hosts, Dj’s, Artists, Producers… etc.</p>
+				<h3>Goal</h3>
+				<p>The main objective of this website is to connect with businesses and musicians looking to book events, production, and studio recording packages. The secondary purpose is to display information about  services A.N.C.  currently provides.</p>
+				<h2>Persona</h2>
+			</main>
+			<article>
+				<h3>Name</h3>
+				<p>Billy Bill Billerson</p>
+				<h3>Age</h3>
+				<p>35</p>
+				<h3>Profession</h3>
+				<p> Manager for  band “Bill's Bill Bills”, an up and coming Pop Rock act. Billy handles the bands day to day operations for the band, as well as booking studios for them to create and record music.</p>
+				<h3>Technology</h3>
+				<p>Samsung Galaxy Note 7, and Macbook Pro 15” user. These tools allow him to be flexible to his bands often "on the fly" schedule and needs.</p>
+				<h3>Attitudes and Behaviors</h3>
+<p>Billy is a respected manager in the entertainment industry of 10+ years. He expects business to be handled as agreed, and with professionalism. Being a seasoned professional he prides himself on making sound decisions. He has a known distaste for bad business from the organizations he deals with.  While the band is the main act, Billy is the man that makes it all happen for the band. </p>
+				<h3>Frustrations and Needs</h3>
+				<p>Due to a scheduling conflict Billy is unable to get the band back to record at their normal studio. They need a quality recording space, and staff to complete the project before the opening night in less than two months.</p>
+				<h3>Goals</h3>
+				<p>Billy needs a facility for his band to record the backing tracks for the Bill’s Bill Bills, on their upcoming summer album release, “Final Notice”. He is looking for a business with a strong, Particular set of skills as well as ease of contact. Finding a high quality facility is his Top priority</p>
+				<h3>Use Case</h3>
+			</article>
+				<article>
+				<p> As a manager he need to quickly find a quality space for my band to record music.</p>
+				<h2>Use Case</h2>
+				<p>Reaching out to his contacts in the Southwest region, Billy B was told to contact A.N.C. Studios through their website for his recording needs. Billy visits the site and fills out the contact forms,  convinced A.N.C. Studios is the best place for the Bill’s Bill Bills to finish their music.</p>
+			</article>
+
+		</body>
+</html>
